912adc6de5faf0b61b459437b5f9a4542337f83a
krishnendu-krish <krishnendukrish6535@gmail.com>
2026-03-25 16:12:52 +0530

    first commit

--- core/serializers.py
+++ core/serializers.py
@@ -5,3 +5,13 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+    def validate_name(self, value):
+        if not value:
+            raise serializers.ValidationError("Name is required")
+        return value
+    def validate_email(seld, value):
+        if Student.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
+        
